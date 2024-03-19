518d109719d47a46d68a3bb4f17026380499acf5
Daniel Claesson <dacl@rocketmail.com>
2024-03-19 08:06:30 +0100

    First commit Lab2. Lab1 finished (hopefully).

--- Lab/Lab_1_load_model.py
+++ Lab/Lab_1_load_model.py
@@ -1,9 +1,11 @@
 import joblib
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+import os
 
-X_test = pd.read_csv("Lab_1_test_saples.csv")
-my_model = joblib.load('Lab_1_production_model_svc.pkl')
-my_model.predict(X_test)
-print(my_model)
-
+os.chdir('C://Users//danie//Documents//GitHub//Machine_Learning_Daniel_Claesson//Lab')
+X_test = pd.read_csv('Lab_1_test_samples.csv')
+my_model = joblib.load('Lab_1_production_model_forest.joblib')
+prediction = my_model.predict(X_test)
+df = pd.DataFrame(prediction, columns=['prediction'])
+df.to_csv('Lab_1_predictions.csv', index=False)
