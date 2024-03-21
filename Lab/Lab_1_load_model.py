cca7a9f2045c0485e5fa15947ce2c62a9f21900b
Daniel Claesson <dacl@rocketmail.com>
2024-03-21 18:23:59 +0100

    Updated production model and csv output file with probabilities.

--- Lab/Lab_1_load_model.py
+++ Lab/Lab_1_load_model.py
@@ -10,6 +10,9 @@ X_test = test_samples.drop(['cardio'], axis=1)
 my_model = load('Lab_1_production_model_forest.joblib')
 prediction = my_model.predict(X_test)
 probability = my_model.predict_proba(X_test)
-df = pd.DataFrame([prediction, probability], columns=['prediction','probability'])
 
-#df.to_csv('Lab_1_predictions.csv', index=False)
+df = pd.DataFrame(prediction, columns=['prediction'])
+df['probability class 0'] = pd.DataFrame(probability[:,0])
+df['probability class 1'] = pd.DataFrame(probability[:,1])
+
+df.to_csv('Lab_1_predictions.csv', index=False)
