5173a572d4fa5a26e34d3d81dff932d2f4f6639b
Daniel Claesson <dacl@rocketmail.com>
2024-03-22 09:32:43 +0100

    KNN as best prod model. Updates to main ipynb an py files.

--- Lab/Lab_1_load_model.py
+++ Lab/Lab_1_load_model.py
@@ -1,13 +1,13 @@
 from joblib import load
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 import os
 
 os.chdir('C://Users//danie//Documents//GitHub//Machine_Learning_Daniel_Claesson//Lab')
 test_samples = pd.read_csv('Lab_1_test_samples.csv')
 X_test = test_samples.drop(['cardio'], axis=1)
 
-my_model = load('Lab_1_production_model_forest.joblib')
+my_model = load('Lab_1_production_model_knn.joblib')
 prediction = my_model.predict(X_test)
 probability = my_model.predict_proba(X_test)
 
